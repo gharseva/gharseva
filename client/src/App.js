a22b56b47d4ad1b03094d9133dc96e1f3613019a
gharseva <gharseva000@gmail.com>
2023-03-27 14:41:00 +0530

    testing loading screen

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./styles/theme";
 import Routes from "./Routes";
+import "./styles/app.css";
 
 export const UserContext = createContext({});
 
@@ -32,7 +33,20 @@ function App() {
     <UserContext.Provider value={userSession}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        {loading ? <>LOADING...</> : <Routes />}
+        {loading ? (
+          <>
+            <div class="spinner-box">
+              <div class="leo-border-1">
+                <div class="leo-core-1"></div>
+              </div>
+              <div class="leo-border-2">
+                <div class="leo-core-2"></div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <Routes />
+        )}
       </ThemeProvider>
     </UserContext.Provider>
   );

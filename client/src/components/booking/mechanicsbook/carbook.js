905e9a5c42815e8c9a3d6d49e45b4b9ac67c0eba
gharseva <gharseva000@gmail.com>
2023-03-27 15:07:55 +0530

    form submit alert update

--- client/src/components/booking/mechanicsbook/carbook.js
+++ client/src/components/booking/mechanicsbook/carbook.js
@@ -32,7 +32,9 @@ const Carbook = () => {
     settypeodproblem(currentValue4);
   };
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // Prevent the form from submitting normally
+
     const res = await fetch("/api/mbooking", {
       method: "POST",
       headers: {
@@ -49,7 +51,14 @@ const Carbook = () => {
       }),
     });
     await res.json();
-    alert("Your order has been booked");
+    if (res.ok) {
+      alert("Your order has been booked");
+      window.location.href = "/"; // Redirect to home page
+    } else {
+      const error = await res.json();
+      alert("There was some error. Fill the form again.");
+      window.location.reload();
+    }
   };
 
   return (
